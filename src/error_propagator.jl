02ec6f0b2b0314e2a6ae81fae04636a16b5f5653
Hannah Boinowitz <hannah.boinowitz@web.de>
2022-02-25 18:29:19 +0100

    Put error propagation method in String-Variable

--- src/error_propagator.jl
+++ src/error_propagator.jl
@@ -1,25 +1,37 @@
 using SymPy
 
-function get_propagated_error_string(f::Sym, error_variables::Vector{Sym}, gaussian::Bool)
+function get_propagated_error_string(f::Sym, error_variables::Vector{Sym}, method::String, 
+                                     execute_derivative::Bool = true)
+
+    if !(method in ["lin", "gaussian"]) 
+        throw(ArgumentError("Error Propagation Method must be one of `lin` or `gaussian`."))
+    end
+
     index = 1
     num_of_error_variables = length(error_variables)
 
     error = ""
-    if gaussian
+    if method == "gaussian"
         error = "\\sqrt{"
     end
 
     for i in error_variables
-        if gaussian
-            error *= "\\left($(sympy.latex(diff(f, i)))\\cdot\\Delta $(sympy.latex(i))\\right)^2"
+        if execute_derivative
+            derivative = sympy.latex(diff(f, i))
         else
-            error *= "\\left|$(sympy.latex(diff(f, i)))\\right|\\cdot\\Delta $(sympy.latex(i))\\right"
+            derivative = sympy.latex(sympy.Derivative(f, i))
+        end
+
+        if method == "gaussian"
+            error *= "\\left($(derivative)\\cdot\\Delta $(sympy.latex(i))\\right)^2"
+        else
+            error *= "\\left|$(derivative)\\right|\\cdot\\Delta $(sympy.latex(i))"
         end
 
         if index < num_of_error_variables
             error *= " + "
         end
-        if index == num_of_error_variables && gaussian
+        if index == num_of_error_variables && method == "gaussian"
             error *= "}"
         end
         index += 1
@@ -28,22 +40,34 @@ function get_propagated_error_string(f::Sym, error_variables::Vector{Sym}, gauss
 end
 
 function get_propagated_error_function(f::Sym, error_variables::Vector{Sym}, 
-                                      errors::Vector{Sym}, gaussian::Bool)
-    index = 1
+                                      errors::Vector{Sym}, method::String, execute_derivative::Bool = true)
+    
+    if !(method in ["lin", "gaussian"]) 
+        throw(ArgumentError("Error Propagation Method must be one of `lin` or `gaussian`."))
+    end
 
+    index = 1
     @vars error
     error = 0
 
     for i in error_variables
-        if gaussian
-            error += (sympy.Derivative(f, i) * errors[index]) ^ 2
+        if execute_derivative
+            derivative = diff(f, i)
         else
-            error += (sympy.Derivative(f, i) * errors[index])
+            derivative = sympy.Derivative(f, i)
+        end
+
+        if method == "gaussian"
+            error += (derivative * errors[index]) ^ 2
+        else
+            error += (derivative * errors[index])
         end
         index += 1
     end
-    if gaussian
+
+    if method == "gaussian"
         return sympy.sqrt(error)
     end
+    
     return error
 end
