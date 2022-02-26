73e5083ad2efe2f2eb18738a305b9fcf40d87a4e
Hannah Boinowitz <hannah.boinowitz@web.de>
2022-02-26 10:53:22 +0100

    Fix based on failing test

--- src/latex_utils.jl
+++ src/latex_utils.jl
@@ -2,6 +2,6 @@ function enclose(raw_string::String; inline::Bool=false, numbered::Bool=true)
     if inline
         return "\$$(raw_string)\$"
     else
-        return "\\begin{equation$(numbered ? '*' : "")}\n\t$(raw_string)\n\\end{equation$(numbered ? '*' : "")}"
+        return "\\begin{equation$(numbered ? "" : '*')}\n\t$(raw_string)\n\\end{equation$(numbered ? "" : '*')}"
     end
 end
